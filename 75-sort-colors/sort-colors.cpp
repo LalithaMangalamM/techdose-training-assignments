a750abad7db538dfa733ec1cb470df22ac9c61cf
LalithaMangalamM <89401899+LalithaMangalamM@users.noreply.github.com>
2023-08-01 12:31:06 +0530

    Time: 4 ms (25.99%) | Memory: 8 MB (98.77%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int partition(vector<int>&nums, int low, int high)
+    {
+        int i=low;
+        int p = nums[high];
+        for(int j=low;j<high;j++)
+        {
+            if(nums[j]<=p)
+            {
+        
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+        }
+        int temp = nums[i];
+        nums[i] = nums[high];
+        nums[high] = temp;
+        return i;
+    }
+
+    void quick(vector<int>&nums, int low, int high)
+    {
+        if(low<high)
+        {
+            int p = partition(nums,low,high);
+            quick(nums,low,p-1);
+            quick(nums,p+1,high);
+        }
+    }
+    void sortColors(vector<int>& nums) {
+       quick(nums, 0, nums.size()-1);
+    }
+};

529bb0ca75a99f2200b9afa6ef5e5dc8de212281
LalithaMangalamM <lalli155003@gmail.com>
2024-02-08 14:59:21 +0530

    Time: 217 ms (46.91%) | Memory: 152.3 MB (20.47%) - LeetSync

--- array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // brute approach
-        // vector<int>ans(nums.size()-k+1);
-
-        // for(int i=0;i<=nums.size()-k;i++)
-        // {
-        //     int max = nums[i];
-        //     for(int j=1;j<k;j++)
-        //     {
-        //         if(max<nums[i+j])
-        //         max=nums[i+j];
-        //     }
-        //     ans[i] = max;
-        //     max=0;
-        // }
-        // return ans;
-        //pq
         priority_queue<pair<int,int>> pq;
         vector<int>res;
         for(int i=0;i<nums.size();i++)

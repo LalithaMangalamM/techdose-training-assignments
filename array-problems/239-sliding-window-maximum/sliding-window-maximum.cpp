02e31475b0f84071aef9dfdbfbeaeafcdefe1e4f
LalithaMangalamM <lalli155003@gmail.com>
2024-02-08 14:54:31 +0530

    Time: 166 ms (92.95%) | Memory: 132.2 MB (97.66%) - LeetSync

--- array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -16,18 +16,33 @@ public:
         //     max=0;
         // }
         // return ans;
-        priority_queue<pair<int,int>> pq;
-        vector<int>res;
-        for(int i=0;i<nums.size();i++)
-        {
-            while(!(pq.empty()) && pq.top().second<=i-k){
-                pq.pop();
-            }
-            pq.push({nums[i],i});
-            if(i >= k-1){
-                res.push_back(pq.top().first);
-            }
+        // priority_queue<pair<int,int>> pq;
+        // vector<int>res;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     while(!(pq.empty()) && pq.top().second<=i-k){
+        //         pq.pop();
+        //     }
+        //     pq.push({nums[i],i});
+        //     if(i >= k-1){
+        //         res.push_back(pq.top().first);
+        //     }
+        // }
+        // return res;
+         int n=nums.size();
+        deque<int> max_idx;//Double-ended queue storing index for max
+        vector<int> ans(n-k+1);
+
+        for(int i=0; i<n; i++){
+            while(!max_idx.empty() && nums[i]>=nums[max_idx.back()]) 
+                max_idx.pop_back();// pop back the indexes for smaller ones
+            max_idx.push_back(i);  // push back the index for larger one
+
+            if (max_idx.front()==i-k) // index=i-k should not in the window
+                max_idx.pop_front(); 
+            if (i>=k-1)
+                ans[i-k+1]=nums[max_idx.front()]; //Max element for this window
         }
-        return res;
+        return ans;
     }
 };

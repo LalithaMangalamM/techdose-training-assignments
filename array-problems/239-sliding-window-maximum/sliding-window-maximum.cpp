27587da722fabe60471b167959e9974991cf55e8
LalithaMangalamM <lalli155003@gmail.com>
2024-02-08 14:56:28 +0530

    Time: 191 ms (78.11%) | Memory: 152.3 MB (19.77%) - LeetSync

--- array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ array-problems/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -16,6 +16,7 @@ public:
         //     max=0;
         // }
         // return ans;
+        //pq
         priority_queue<pair<int,int>> pq;
         vector<int>res;
         for(int i=0;i<nums.size();i++)
@@ -29,20 +30,5 @@ public:
             }
         }
         return res;
-        //  int n=nums.size();
-        // deque<int> max_idx;//Double-ended queue storing index for max
-        // vector<int> ans(n-k+1);
-
-        // for(int i=0; i<n; i++){
-        //     while(!max_idx.empty() && nums[i]>=nums[max_idx.back()]) 
-        //         max_idx.pop_back();// pop back the indexes for smaller ones
-        //     max_idx.push_back(i);  // push back the index for larger one
-
-        //     if (max_idx.front()==i-k) // index=i-k should not in the window
-        //         max_idx.pop_front(); 
-        //     if (i>=k-1)
-        //         ans[i-k+1]=nums[max_idx.front()]; //Max element for this window
-        // }
-        // return ans;
     }
 };

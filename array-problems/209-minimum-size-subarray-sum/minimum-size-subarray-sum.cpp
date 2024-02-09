8c64f263a9f48c76da9d9823d738a7cce450a1f9
LalithaMangalamM <lalli155003@gmail.com>
2024-02-09 15:43:29 +0530

    Time: 14 ms (99.53%) | Memory: 30.7 MB (7.09%) - LeetSync

--- array-problems/209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ array-problems/209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+         ios_base::sync_with_stdio(false);
+          std::cin.tie(0);
         int sum=0,left=0,right=0,window=INT_MAX;
         int n=nums.size();
         while(right<n)

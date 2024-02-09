cfc0a9346a0bc5d042ac943f89f028ddcd0842b0
LalithaMangalamM <lalli155003@gmail.com>
2024-02-09 15:50:33 +0530

    Time: 13 ms (99.61%) | Memory: 30.7 MB (7.09%) - LeetSync

--- array-problems/209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ array-problems/209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        std::cin.tie(0);
-        int sum = 0, left = 0, right = 0, window = INT_MAX;
-        int n = nums.size();
-        while (right < n) {
-            sum += nums[right];
-            while (sum >= target) {
-                window = min(window, right + 1 - left);
-                sum -= nums[left++];
-            }
+         ios_base::sync_with_stdio(false);
+          std::cin.tie(0);
+        int sum=0,left=0,right=0,window=INT_MAX;
+        int n=nums.size();
+        while(right<n)
+        {
+            sum+=nums[right];
+            if(sum>=target)
+            {
+                while(sum>=target)
+                {
+                    sum-=nums[left];
+                    left++;
+                }
+                window=min(window,right-left+2);
+            }            
             right++;
         }
-        return window == INT_MAX ? 0 : window;
+        return window==INT_MAX?0:window;
+        
     }
 };

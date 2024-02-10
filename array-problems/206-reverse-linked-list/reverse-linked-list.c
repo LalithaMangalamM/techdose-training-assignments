9a275e255512f7d376016b303426c15f77ed7c00
LalithaMangalamM <lalli155003@gmail.com>
2024-02-10 10:02:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (81.32%) - LeetSync

---
+++ array-problems/206-reverse-linked-list/reverse-linked-list.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    if(head == NULL) return NULL; 
+
+   if(head->next == NULL) return head; 
+
+   struct node* rest = reverseList(head->next); 
+
+   head->next->next = head; 
+
+   head->next = NULL; 
+
+   return rest;
+}
